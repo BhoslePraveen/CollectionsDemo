a8a63f9025bd050415e31880456cef3dd16e5a04
praveenbhosle <praveen.bhosle5@gmail.com>
2023-01-07 21:04:33 +0530

    Added file for arraylist

---
+++ src/com/velocity/arraylist/ArrayListDemo.java
@@ -0,0 +1,22 @@
+package com.velocity.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("Pune");
+		list.add("Mumbai");
+		list.add("Bangalore");
+		list.add("Delhi");
+		// System.out.println(list);
+		for (String s : list) {
+			System.out.println(s);
+		}
+
+	}
+
+}
